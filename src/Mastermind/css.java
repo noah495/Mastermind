07e1ea4e3047aa8d06d26aaafd80d94e559e1700
noah495 <noah.buchs@outlook.com>
2020-08-05 16:06:38 +0200

    Zufallsgenerator versucht

--- src/Mastermind/css.java
+++ src/Mastermind/css.java
@@ -1,7 +1,11 @@
 package Mastermind;
-
+import java.util.Random;
 public class css {
    public static void main(String[] args){
+       Random farbe = new Random();
+       String[] farben = {"Blau", "Rot", "Gelb", "Grün", "Schwarz", "Weiss", "Lila", "Orange"};
+       farbe = 1 + farben.nextString(7); 
+
 
 
         
