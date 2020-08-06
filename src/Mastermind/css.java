c10d6bed2947fe0caf06ba0ece68ac810998e864
noah495 <noah.buchs@outlook.com>
2020-08-06 12:50:00 +0200

    commit 12.49

--- src/Mastermind/css.java
+++ src/Mastermind/css.java
@@ -2,8 +2,9 @@ package Mastermind;
 import java.util.Random;
 public class css {
    public static void main(String[] args){
-       Random farbe = new Random();
+
        String[] farben = {"Blau", "Rot", "Gelb", "Grün", "Schwarz", "Weiss", "Lila", "Orange"};
+       Random farbe = new Random();
        farbe = 1 + farben.nextString(7);
 // command k (nachricht nicht löschen)
 
