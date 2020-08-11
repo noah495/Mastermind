3fdb5e4ea982196da4a21f5e3444119fcfa8048d
Yanic Ruoss <yanic.ruoss@css.ch>
2020-08-11 14:32:06 +0200

    finalno

--- src/Mastermind/css.java
+++ src/Mastermind/css.java
@@ -10,7 +10,7 @@ public class css {
 
         System.out.println("Guten Tag");
         System.out.println("Wie lautet dein Name?");
-       String eingabe = sc.nextLine();
+        String eingabe = sc.nextLine();
         System.out.println("Hallo " + eingabe + ", weisst du wie dieses Spiel geht?");
         String input = sc.nextLine();
         if (input.equals("Nein") || input.equals("nein")) {
@@ -25,7 +25,7 @@ public class css {
             System.out.println("Und wie viele die richtige Farbe haben, aber an der falschen Position sind");
             TimeUnit.SECONDS.sleep(3);
         }
-        System.out.println("Im ganzen gibt es 8 Farben; Blau, Rot, Gelb, Grün, Schwarz, weiss, Lila, Orange.");
+        System.out.println("Im ganzen gibt es 8 Farben: Blau, Rot, Gelb, Grün, Schwarz, weiss, Lila, Orange.");
         TimeUnit.SECONDS.sleep(3);
         System.out.println("Das Ziel ist die 4 Farben rauszufinden.");
         TimeUnit.SECONDS.sleep(3);
@@ -41,28 +41,59 @@ public class css {
             String[] benutzerEingaben = new String[4];
             String[] farben = {"blau", "rot", "gelb", "grün", "schwarz", "weiss", "lila", "orange"};
             Random random = new Random();
-            String[] generiertefarbe = new String[4];
+            String[] generiertefarbe = {"blau", "blau", "blau", "rot"};//;new String[4];
 
             for (int x = 1; x <= 12; x++) {
 
-                for (int index = 0; index <= 3; index++) {
-                    generiertefarbe[index] = farben[random.nextInt(farben.length)];
-                   // System.out.println(generiertefarbe[index]);
+              //  for (int index = 0; index <= 3; index++) {
+                  //  generiertefarbe[index] = farben[random.nextInt(farben.length)];
+                    // System.out.println(generiertefarbe[index]);
                     // um lösung z zeigen
-                }
+               // }
+
                 for (x = 1; x <= 12; x++) {
                     System.out.println("Versuch " + x);
                     System.out.println("");
                     for (int index = 0; index <= 3; index++) {
                         System.out.println("Farbe " + index);
                         benutzerEingaben[index] = sc.nextLine().toLowerCase();
+                        boolean istEingabeKorrekt = true;
+                while (istEingabeKorrekt){
+                        switch (istEingabeKorrek) {
+                            case "blau":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "rot":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "gelb":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "grün":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "schwarz":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "weiss":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "lila":
+                                istEingabeKorrekt = false;
+                                break;
+                            case "orange":
+                                istEingabeKorrekt = false;
+                                break;
+                            default:
+                                System.out.println("Bitte achte dich auf die Schreibweise.");
+                                System.out.println("Eingabe des Versuches: ");
+                                benutzerEingaben[index] = sc.nextLine().toLowerCase();
+                                break;
+                        }
+                        }
+                        //     while  (!benutzerEingaben[index].toLowerCase().equals(farben[]))
+                        // }
 
-
-                      while  (!benutzerEingaben[index].equals(generiertefarbe[index])) {
-                          System.out.println("Bitte achte dich auf die Schreibweise.");
-                          System.out.println("Eingabe des Versuches: ");
-                          benutzerEingaben[index] = sc.nextLine().toLowerCase();
-                      }
                     }
 
 //        System.out.println("Analysieren");
@@ -152,10 +183,10 @@ public class css {
 //
 //
 
-
                     }
 
                 }
+
             }
         }
     }
